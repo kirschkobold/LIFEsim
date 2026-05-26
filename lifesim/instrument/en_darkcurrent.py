b48b83e072dd999984ddde6e647af808867ed835
kseliner <kseliner@ethz.ch>
2026-05-26 10:48:13 +0200

    added specific spectral resolution value for dark current

--- lifesim/instrument/en_darkcurrent.py
+++ lifesim/instrument/en_darkcurrent.py
@@ -51,7 +51,7 @@ class ElectronNoiseDarkCurrent(ElectronNoiseDetectorModule):
 
         # read data on detector
         dc_per_pix = self.data.options.array['dc_per_pix']
-        total_pixels = self.data.options.array['pix_per_wl'] * len(self.data.inst['wl_bins']) # minimum number of detector pixels (nyquist rate)
+        total_pixels = self.data.options.array['pix_per_wl'] * self.data.options.array['spec_res_detector'] # minimum number of detector pixels (nyquist rate)
 
         # calculate total dark current noise
         dc_leak = np.full(self.data.inst['wl_bins'].shape, dc_per_pix * total_pixels)
