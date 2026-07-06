a92dc6542f57ac4746c1bb1c23f8d82fb07dd84c
kseliner <kseliner@ethz.ch>
2026-07-06 13:52:33 +0200

    specified number of jobs running in slurm job for optimization

--- lifesim/analysis/submit.py
+++ lifesim/analysis/submit.py
@@ -227,6 +227,10 @@ def run(config_path: str, steps: list[int] | None = None):
                 f.write(f"{merged_catalog_names},{basepath}/{merged_catalog_names}/{endpath}\n")
         '''
 
+        optjobs_csv_path = merge_folder / "config_files" / "optimizer_jobs.csv"
+        with open(optjobs_csv_path, "r") as f:
+            n_jobs = sum(1 for line in f if line.strip())
+
         template_masterlaunch = BashTemplate(read_template("master_launch_template.slurm.sh"))
         content = template_masterlaunch.substitute(
             job_name     = f"{today}_optimizer",
@@ -234,7 +238,8 @@ def run(config_path: str, steps: list[int] | None = None):
             optjobs_path = merge_folder / "config_files" / "optimizer_jobs.csv",
             python_run   = merge_folder / "config_files" / "launch_optimizer.py",
             venv_path    = venv_path,
-            queue        = queue)
+            queue        = queue, 
+            n_jobs       = n_jobs)
         (merge_folder / "config_files" / "master_launch.slurm.sh").write_text(content)
 
         opt_job_id = submit(
--- lifesim/analysis/templates/master_launch_template.slurm.sh
+++ lifesim/analysis/templates/master_launch_template.slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=@queue
 #SBATCH --job-name=@job_name
-#SBATCH --array=1-7%7        # Run tasks 1 through 7, allow 7 to run at once
+#SBATCH --array=1-@n_jobs%@n_jobs        # Run tasks 1 through N, allow N to run at once
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=22
 #SBATCH --mem-per-cpu=4G
